4b7fa7f5663332e58dd779a9b2dba83e29d352b5
matthewmcnulty <mattjoe182@gmail.com>
2022-05-27 17:53:18 +0100

    changed folders

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,13 +1,15 @@
 import requests
 
 # endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/"
 
 get_response = requests.get(endpoint, json={"query": "Hello world"}) # HTTP Request
-# print(get_response.text) # print source code
+print(get_response.text) # print source code
+print(get_response.status_code)
 
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
 # JavaScript Object Notation ~ Python Dict
 # print(get_response.json())
-print(get_response.status_code)
+# print(get_response.status_code)
