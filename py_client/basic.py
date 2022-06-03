6a85ce9bc45437b98eec44d8111bb8e672aa67e9
matthewmcnulty <mattjoe182@gmail.com>
2022-06-03 16:26:19 +0100

    Ingest Data with Django Rest Framework Views

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/" #"http://localhost:8000/"
 
-get_response = requests.post(endpoint, json={"title": "Hello world" }) # HTTP Request
+get_response = requests.post(endpoint, json={"title": "Abc123", "content": "Hello world", "price": "abcd1234"}) # HTTP Request
 # print(get_response.headers)
 # print(get_response.text) # print source code
 # print(get_response.status_code)
